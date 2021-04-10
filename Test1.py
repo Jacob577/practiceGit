19ef1123ebc602fe739dcf5697f306f48be5a587
Jacob577 <deltora.ericsson@gmail.com>
2021-04-10 17:04:58 +0200

    Initial commit

---
+++ Test1.py
@@ -0,0 +1,10 @@
+class Test1:
+    '''
+    This is an advent of code challange
+    coop to practice Git
+    '''
+    def __init__(self):
+        pass
+
+    def hello(self):
+        print("Hello from Test1")
---
+++ main.py
@@ -0,0 +1,14 @@
+from Test1 import Test1
+class Main:
+    def __init__(self, name):
+        self.name = str(name)
+
+    def run(self):
+        print("Your name is {}".format(self.name))
+
+
+if __name__ == '__main__':
+    Main(name="Jacob och Lucas").run()
+    # Test1().hello()
+
+
